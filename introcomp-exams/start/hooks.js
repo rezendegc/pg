a8de0a0fe00499a70f1e660cb4d806ba7d6a6b0d
Gabriel Rezende <gabrielcdrezende@gmail.com>
2019-10-28 17:20:15 -0300

    countdown on waiting screen

--- introcomp-exams/start/hooks.js
+++ introcomp-exams/start/hooks.js
@@ -7,21 +7,8 @@ hooks.after.providersBooted(() => {
     const View = use('View')
 
     const moment = use('moment')
+
     View.global('formatDate', (value, pattern) => {
         return moment(value).format(pattern)
-    })
-
-    View.global('countdown', (schedule) => {
-        const start = moment(schedule.start_datetime)
-        const registerTime = schedule.register_time.split(':')
-        start.add(registerTime[0], 'h')
-        start.add(registerTime[1], 'm')
-        start.add(registerTime[2], 's')
-
-        return console.log("hey!")
-
-        return start.diff(moment())
-
-        // return start.diff(moment())
     })
 })
