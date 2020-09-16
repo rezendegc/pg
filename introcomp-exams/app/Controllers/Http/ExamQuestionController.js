11cec623978dde4d3a03f6377ffb62d168afb900
Gabriel Rezende <gabrielcdrezende@gmail.com>
2020-09-16 19:52:46 -0300

    only allow check answer if exam is not finished

--- introcomp-exams/app/Controllers/Http/ExamQuestionController.js
+++ introcomp-exams/app/Controllers/Http/ExamQuestionController.js
@@ -4,13 +4,14 @@ const moment = require('moment');
 
 class ExamQuestionController {
     async update({ params, auth, request, response }) {
-        // TODO: VERIFICAR SEMPRE SE A PROVA DO USUÁRIO AINDA É VÁLIDA
         const { answer } = request.all()
         const { id } = params
 
         if (!id) return response.status(400)
 
         const exam = await auth.user.exam().first()
+
+        if (exam.status === 'FINISHED') return response.status(401);
 
         if (Number(answer) < 0 || Number(answer) > 5) return response.status(400)
 
